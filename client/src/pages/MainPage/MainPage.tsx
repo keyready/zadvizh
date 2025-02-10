397ab13e11679dc6ab6de292963b6258a9f3b27c
keyready <vip.korchak@list.ru>
2025-02-10 10:01:15 +0300

    tg auth #5

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,26 +1,38 @@
 import {LoginButton} from "@telegram-auth/react";
-import {useNavigate, useParams} from "react-router";
+import {useNavigate, useSearchParams} from "react-router-dom";
 
 export const MainPage = () => {
     const navigate = useNavigate();
-    const { ref } = useParams<{ ref: string }>();
+    const [params] = useSearchParams();
+
+    if (!params.get('ref')) {
+        return (
+            <section className="bg-main-gradient flex h-screen w-full items-center justify-center">
+                <div className="min-w-1/3 min-h-64 flex flex-col justify-center items-center rounded-xl bg-primary p-4">
+                    <h1 className="text-center text-3xl font-bold text-black dark:text-white">
+                        Без приглашения сюда не попасть :(
+                    </h1>
+                </div>
+            </section>
+        )
+    }
 
     return (
         <section className="bg-main-gradient flex h-screen w-full items-center justify-center">
-            <div className="min-w-1/3 min-h-1/3 flex flex-col items-center rounded-xl bg-primary p-4">
+            <div className="min-w-1/3 min-h-64 flex flex-col items-center rounded-xl bg-primary p-4">
                 <h1 className="text-center text-3xl font-bold text-black dark:text-white">
                     Добро пожаловать в Движ
                 </h1>
                 <h2 className="mt-10 text-center text-2xl text-black dark:text-white">
                     Вы попали сюда не случайно!
                 </h2>
-                <h3 className="text-center text-xl text-black dark:text-white">
+                <h3 className="mb-10 text-center text-xl text-black dark:text-white">
                     Для получения доступа к чату, авторизуйтесь через Telegram
                 </h3>
                 <LoginButton
                     cornerRadius={10}
-                    onAuthCallback={() => navigate(`/auth/continue?ref=${ref}`)}
-                    botUsername='zadvizh_assistant_bot'
+                    onAuthCallback={() => navigate(`/auth/continue?ref=${params.get('ref') || ''}`)}
+                    botUsername={import.meta.env.VITE_BOT_USERNAME}
                 />
             </div>
         </section>
